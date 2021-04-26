140e1b4d93e502ba25f08fd649268148c6f2c87c
yiroO <yirooyair@gmail.com>
2021-04-26 08:36:25 -0500

    se añade icono

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  data = {
+    name: 'FERNANDO YAIR VALENCIA NELO',
+    titulo: 'Desarrollador FrontEnd Jr.',
+    email:  'ingfernandoyair@gmail.com',
+    linke: 'fernandoyair',
+    tel: '2381732107',
+    desc: 'Soy una persona joven, dinámica, con facilidad de integración y con muy buena habilidad de comunicación. Estoy buscando traer mis habilidades aprendidas durante mis estudios, pasantías y experiencia laboral. Mi objetivo es seguir aprendiendo y creciendo profesionalmente. He adquirido conocimientos en la parte del FrontEnd con JavaScript, HTML5, CSS3 y Angular. En la parte del BackEnd con Java, Spring Boot, Spring MVC y en base de datos como MySQL, SqlServer y Oracle.'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
